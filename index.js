8cdec0f13f3d58080cf502916a83ba814b34cda5
Barron Eskind <35750926+bearman222@users.noreply.github.com>
2022-12-20 17:33:24 +0000

    fixed prompts, added debugging.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 var readlineSync = require('readline-sync');
 console.log('readline-sync demo');
-var theAnswer = '10';
+var theAnswer = Math.round(Math.random() * 100);
 // Wait for user's response.
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
 
 var playGame = readlineSync.question('Do you want to play a game?');
 if (playGame === "no") {
-    console.log('oh no');
+    console.log('Okay, maybe next time.');
 } else {
     console.log('Good, let\'s play guess a number!');
-    console.log('I\'m thinking of a number between 1-100');
+    // console.log('(Hint:' + theAnswer + '\)');
+    console.log('I\'m thinking of a number between 1-100...');
     // Handle the secret text (e.g. password).
     let theGuess = ''
 
     while (theGuess != theAnswer) {
         theGuess = readlineSync.question('What #: ');
-        if (theGuess != theAnswer) {
-            console.log("Try Again.")
+        if (theGuess > theAnswer) {
+            console.log("Lower!");
+        }
+        else if (theGuess < theAnswer) {
+            console.log("Higher!");
+        }
+        else {
+            console.log("You've guessed it! " + theAnswer);
         }
     }
-    console.log("You Guessed Right!")
 }
